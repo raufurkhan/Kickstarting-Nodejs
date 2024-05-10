7b7ff8494ec9ccf730b2ca851250e2820c5f9382
raufurkhan <raufurkhan82@gmail.com>
2024-05-10 11:24:54 +0530

    Lets get started with Express

--- form.js
+++ form.js
@@ -1,13 +1,28 @@
 const http = require("http");
-const port = 5000;
+//const port = 5000;
+
+const express=require('express')
+
+const app= express()
 
 
-const routes=require("./routes")
-const server = http.createServer(routes)
-    
+app.use((req,res,next)=>{
+console.log("in middleware")
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("in another middleware")
+    res.send('<h1> hello from express</h1>')
+})
+//const routes=require("./routes")
+//const server = http.createServer(routes)
+const server = http.createServer(app)
 
    
 
-server.listen(port, () => {
-    console.log("Listening at port " + port);
-});
+//server.listen(port, () => {
+ //   console.log("Listening at port " + port);
+//});
+
+server.listen(3000)
